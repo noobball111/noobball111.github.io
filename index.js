9bcd23be12244a5fad161d9b14a7537f68991e6f
Tracck <85881232+Tracck@users.noreply.github.com>
2023-05-22 22:35:33 +0700

    work yes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var count = 0
-document.getElementById("verycool").onclick = function() {
-    console.log("pls senpai work")
-    count += 1;
-    alert(`Your counted to: ${count}`)
+const verycoolButton =  document.getElementById("verycool")
+verycoolButton.onclick = function() {
+    count++;
+    alert(`Your counted to: ${count}`);
 }
