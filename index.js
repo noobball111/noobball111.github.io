f962e6f4ae14a43841269b3809e6b919421ea460
Tracck <85881232+Tracck@users.noreply.github.com>
2023-05-22 22:38:27 +0700

    work pls2

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-var count = 0
-const verycoolButton =  document.getElementById("verycool")
-verycoolButton.onclick = function() {
+var verycoolButton =  document.getElementById("verycool");
+var count = 0;
+function onclick() {
     count++;
     alert(`Your counted to: ${count}`);
+}
+
+if (verycoolButton.addEventListener) {
+    verycoolButton.addEventListener("click", onclick, false);
+} else if (verycoolButton.attachEvent) {
+    verycoolButton.attachEvent('onclick', onclick);
 }
