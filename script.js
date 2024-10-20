e7d33ab7240f6f381519c6da75c030bfc7a779b5
rqp <59442213+noobball111@users.noreply.github.com>
2024-10-20 13:20:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-window.document.getElementById("button").onclick = function(){
-    alert("Hi")
-}
-
-function Video({ video }) {
-    return (
-      <div>
-        <Thumbnail video={video} />
-        <a href={video.url}>
-          <h3>{video.title}</h3>
-          <p>{video.description}</p>
-        </a>
-        <LikeButton video={video} />
-      </div>
-    );
-  }
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 0);
+  };
